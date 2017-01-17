11d92920fdc936d15b8bb5023bd4096f2aaf54f8
Gulchuk Petr <P.Gulchuk@yandex.ru>
2017-01-17 17:57:48 +0500

    Add new lessson and edit readme

---
+++ screencust/lesson_4/welcome.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function(message) {
+  
+  console.log(`Welcome ${message}`);
+  alert(`Welcome ${message}`);
+}
---
+++ screencust/lesson_5/welcome.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(message) {
+
+  console.log(`Welcome ${message}`);
+
+  // debugger;
+  alert(`Welcome ${message}`);
+}
